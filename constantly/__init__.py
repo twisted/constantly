19c982d7956a4bec171c860c1653933c748d465d
Robert-André Mauchin <zebob.m@gmail.com>
2021-12-18 01:52:22 +0100

    Update versioneer.py to 0.21

--- constantly/__init__.py
+++ constantly/__init__.py
@@ -5,9 +5,8 @@ from constantly._constants import (
     NamedConstant, Names, ValueConstant, Values, FlagConstant, Flags
 )
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 __author__  = "Twisted Matrix Laboratories"
 __license__ = "MIT"
